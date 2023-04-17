80209cdf709df727d2f0423dfd55584f02c47ed6
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-04-17 20:24:02 +0000

    Create and activate python venv

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,10 @@
 
 # Fixes: https://github.com/microsoft/vscode-remote-release/issues/7982
 echo "{}" > ~/.docker/config.json
+
+# Virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
+# Development dependencies
+pip install -r dev-requirements.txt
