586b60c74dbb3556056fa0734baa1f1d761224fb
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-04-19 18:57:52 +0000

    Invoke processing step function on upload

--- functions/aws/handle_upload_notification/index.py
+++ functions/aws/handle_upload_notification/index.py
@@ -1,10 +1,17 @@
 import json
+import os
+
+import boto3
+
+
+PROCESS_UPLOADED_OBJECTS_STATE_MACHINE = os.environ['PROCESS_UPLOADED_OBJECTS_STATE_MACHINE']
 
 
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "event": event,
-        }),
-    }
+    s3_object_key = event['Records'][0]['s3']['object']['key']
+
+    sf_client = boto3.client('stepfunctions')
+    sf_client.start_execution(
+        stateMachineArn=PROCESS_UPLOADED_OBJECTS_STATE_MACHINE,
+        input=json.dumps({'objectKey': s3_object_key}),
+    )
