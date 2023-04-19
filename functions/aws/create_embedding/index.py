ad83c96df73de03242a528696b5dc73f27535157
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-04-19 02:16:43 +0000

    Add upload processing flow

---
+++ functions/aws/create_embedding/index.py
@@ -0,0 +1,31 @@
+import json
+# import os
+
+# import boto3
+# from aws_secretsmanager_caching import SecretCacheConfig, SecretCache
+
+
+# OPENAI_API_KEY_SECRET_NAME = os.environ['OPENAI_API_KEY_SECRET_NAME']
+# OPENAI_API_KEY_SECRET_REGION = os.environ['OPENAI_API_KEY_SECRET_REGION']
+
+
+# def create_secret_cache():
+#     client = boto3.client(
+#         service_name='secretsmanager',
+#         region_name=OPENAI_API_KEY_SECRET_REGION,
+#     )
+#     secret_cache_config = SecretCacheConfig()
+#     secret_cache = SecretCache(config=secret_cache_config, client=client)
+#     return secret_cache
+
+
+# secret_cache = create_secret_cache()
+
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "event": event,
+        }),
+    }
