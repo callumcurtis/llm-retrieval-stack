550cb98b1702353d3968862bb7f31ea6892c2516
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-04-20 02:33:10 +0000

    Create boto3 client in lambda initialization

--- functions/aws/get_upload_url/index.py
+++ functions/aws/get_upload_url/index.py
@@ -6,17 +6,10 @@ import boto3
 
 UPLOAD_BUCKET_NAME = os.environ['UPLOAD_BUCKET']
 
+s3_client = boto3.client('s3')
+
 
 def create_presigned_url(client_method, bucket_name, object_key, expiration=120):
-    """Generate a presigned URL to share an S3 object
-
-    :param bucket_name: string
-    :param object_key: string
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Presigned URL as string. If error, returns None.
-    """
-
-    s3_client = boto3.client('s3')
     return s3_client.generate_presigned_url(
         ClientMethod=client_method,
         Params={
