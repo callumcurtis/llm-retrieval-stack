e5f239fdb3d8c18cc1ac1d51ea5d8e0680d54da5
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-04-20 01:20:59 +0000

    Fix upload notification handler event access

--- functions/aws/handle_upload_notification/index.py
+++ functions/aws/handle_upload_notification/index.py
@@ -2,24 +2,33 @@ import json
 import os
 
 import boto3
+from aws_lambda_powertools import Logger
 
 
 TEXT_PROCESSING_STATE_MACHINE = os.environ['TEXT_PROCESSING_STATE_MACHINE']
 UPLOAD_BUCKET = os.environ['UPLOAD_BUCKET']
 
+logger = Logger()
+s3_client = boto3.client('s3')
+sf_client = boto3.client('stepfunctions')
 
+
+@logger.inject_lambda_context()
 def handler(event, context):
-    object_key = event['Records'][0]['s3']['object']['key']
+    sqs_records = event['Records']
+    for sqs_record in sqs_records:
+        sqs_body = json.loads(sqs_record['body'])
+        object_key = sqs_body['Records'][0]['s3']['object']['key']
 
-    s3_client = boto3.client('s3')
-    object = s3_client.get_object(Bucket=UPLOAD_BUCKET, Key=object_key)
-    object_data = object['Body'].read().decode('utf-8')
+        logger.info(object_key=object_key)
 
-    sf_client = boto3.client('stepfunctions')
-    sf_client.start_execution(
-        stateMachineArn=TEXT_PROCESSING_STATE_MACHINE,
-        input=json.dumps({
-            'objectKey': object_key,
-            'text': object_data,
-        }),
-    )
+        object = s3_client.get_object(Bucket=UPLOAD_BUCKET, Key=object_key)
+        object_data = object['Body'].read().decode('utf-8')
+
+        sf_client.start_execution(
+            stateMachineArn=TEXT_PROCESSING_STATE_MACHINE,
+            input=json.dumps({
+                'objectKey': object_key,
+                'text': object_data,
+            }),
+        )
